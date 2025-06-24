fe28dddb2c1bde3cbd249b1edef518f15910e9e5
TASC\Harish <harish@tascoutsourcing.com>
2025-06-24 23:25:28 +0530

    Quest Model created

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone 
 
-# Create your models here.
+
+class Quest(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField(blank = True)
+    date = models.DateField(default=timezone.now)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.date} - {self.title} - {'✅' if self.is_completed else '❌'}"
