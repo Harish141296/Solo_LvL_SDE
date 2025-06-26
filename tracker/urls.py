789102af7b83011c27e31bc6d03d3429da17c681
TASC\Harish <harish@tascoutsourcing.com>
2025-06-26 06:58:09 +0530

    added related JS to record the time and view to store and url to access that view

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('complete/<int:quest_id>/', views.complete_quest, name='complete_quest'),
+    path('save-focus-time/', views.save_focus_time, name='save_focus_time'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Quest 
+from .models import Quest, DailyFocusTime
+import json 
 from django.utils import timezone 
+from django.http import JsonResponse 
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt 
+def save_focus_time(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        seconds = data.get('seconds', 0 )
+        today = timezone.localdate() # IST 
+        obj, _ = DailyFocusTime.objects.get_or_create(date=today)
+        obj.seconds_spend = seconds 
+        obj.save() 
+        return JsonResponse({'status':'success', 'seconds': obj.seconds_spend})
+    
 def home(request):
     today = timezone.now().date()
     quests = Quest.objects.filter(date=today).order_by('id')
